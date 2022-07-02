dd7f6bdeade6bd841e25290293c6059a60236ee0
aaaalmeida <89162654+aaaalmeida@users.noreply.github.com>
2022-07-02 19:25:57 -0300

    Update main.c

--- main.c
+++ main.c
@@ -7,103 +7,9 @@
 
 // #include "cabecalho.h"
 
-// depois joga essas funcoes e constante td no header
 #define MIN3(a, b, c) ((a) < (b) ? ((a) < (c) ? (a) : (c)) : ((b) < (c) ? (b) : (c)))
 #define TAM_MAX_PALAVRA 50
 #define mostraNoTerminal 0
-
-
-/*typedef struct no{
-    // nao vou mais usar lista encadeada :(
-    unsigned char palavraDicionario[TAM_MAX_PALAVRA];
-    struct no *proximoNo;  // aponta para a proxima palavra da lista
-    struct no *proximaLetra; // aponta para a primeira palavra que começa com a proxima letra
-} dicionario;
-*/
-
-/*
-void leituraDicionarioLISTA(FILE *dic, dicionario *comeco){
-// PILHA É UMA MERDA E VAI FAZER O PROGRAMA FICAR LENTO, NÃO VALE A PENA CONTINUAR COM A IDEIA
-// (mas eu vou tentar msm assim)
-
-        Basicamente esta função cria uma fila encadeada
-        lendo as palavras do arquivo dicionario.txt.
-        O ponteiro proximoNo da struct dicionario aponta para a proxima palavra
-        e o ponteiro proximaLetra aponta para a primera palavra que começa com uma nova letra.
-
-
-// ESSA FUNCAO CONSEGUE LER E IMPRIMIR, O PROBLEMA ATE AGORA É QUE ASSIMILAR primeiraNovaLetra
-// À PRIMEIRA PALAVRA COM LETRA NOVA.
-
-    char puxaDicionario[TAM_MAX_PALAVRA];
-    dicionario *aux1, *aux2, *primeiraNovaLetra;
-    aux1 = malloc(sizeof(dicionario));
-    primeiraNovaLetra = malloc(sizeof(dicionario));
-
-    fgets(comeco->palavraDicionario, TAM_MAX_PALAVRA, dic);
-    comeco->proximoNo = aux1;
-    primeiraNovaLetra = comeco; // PROVAVELMENTE O PROBLEMA TA AQ
-    // ESSE PONTEIRO TALVEZ SEJA UM PONTEIRO PRA PONTEIRO????
-
-    // minha ideia é cria palavras chaves que sao as primeiras palavras a comecarem com outra letra
-    // o problema é se o ponteiro auxiliar primeiraNovaLetra é uma copia do original ou o apontamento
-    // if(primeiraNovaLetra->palavraDicionario[0] != aux1->palavraDicionario[0]) comeco->proximaLetra = aux1;
-
-    int i=0;
-    while(fgets(puxaDicionario, TAM_MAX_PALAVRA, dic)){
-        // printf(puxaDicionario);
-
-        if(i=0){
-            i=1;
-            aux1 = malloc(sizeof(dicionario));
-            strcpy(aux1->palavraDicionario, puxaDicionario);
-            aux1->proximoNo = aux2;
-            aux1->proximaLetra = NULL;
-        // printf(aux1->palavraDicionario);
-
-// PROVAVELMENTE VOU TER Q MUDAR ESSE IF
-            if(primeiraNovaLetra->palavraDicionario[0] != aux1->palavraDicionario[0]){
-            primeiraNovaLetra->proximaLetra = aux1;
-            primeiraNovaLetra = aux1;
-            }
-        }
-
-        else{
-            i=0;
-            aux2 = malloc(sizeof(dicionario));
-            strcpy(aux2->palavraDicionario, puxaDicionario);
-            aux2->proximoNo = aux1;
-            aux2->proximaLetra = NULL;
-        // printf(aux2->palavraDicionario);
-
-// ESSE AQ MUDA JUNTO
-            if(primeiraNovaLetra->palavraDicionario[0] != aux2->palavraDicionario[0]){
-            primeiraNovaLetra->proximaLetra = aux2;
-            primeiraNovaLetra = aux2;
-            }
-        }
-    }
-}
-*/
-
-int hamming(int tamanho, unsigned char *s1, unsigned char *s2) {
-    /*  A distância de Hamming entre duas strings de MESMO COMPRIMENTO é
-        o número de posições nas quais elas diferem entre si.
-        Vista de outra forma, ela corresponde ao menor número de substituições necessárias
-        para transformar uma string na outra, ou o número de erros que transformaram uma na outra.
-    */
-    // https://pt.wikipedia.org/wiki/Distância_de_Hamming#Exemplos
-
-    // NAO VOU MAIS USAR ESSA MERDA
-    int distancia = 0;
-    for (int i = 0; i < tamanho; i++) {
-        if (*s1 != *s2)
-            distancia++;
-        s1++;
-        s2++;
-    }
-    return distancia;
-}
 
 int levenshtein(unsigned char *s1, unsigned char *s2) {
     /*  A distância Levenshtein ou distância de edição entre dois "strings"
@@ -254,23 +160,19 @@ int main() {
 
     clock_t start = clock();
 
-    FILE *arq = fopen("textosExemplo\\palavra.txt", "r");  // texto exemplo
+    FILE *arq = fopen("palavra.txt", "r");  // texto exemplo
     testeArquivo(arq);
-    FILE *dic = fopen("textosExemplo\\dicionario.txt", "r"); // banco de palavras
+    FILE *dic = fopen("dicTESTE.txt", "r"); // banco de palavras
     testeArquivo(dic);
-    FILE *arqCopia = fopen("textosExemplo\\resposta.txt", "w+");  // texto formatado
+    FILE *arqCopia = fopen("resposta.txt", "w+");  // texto formatado
     testeArquivo(arqCopia);
-
-    //COMECO DO DICIONARIO NA LISTA ENCADEADA (ideia ruim)
-    // dicionario *comecoLista = malloc(sizeof(dicionario));   
-    // leituraDicionarioLISTA(arq, comecoLista);
 
     unsigned int qtdPalavrasDicionario = achaTamanhoArquivo(dic);
     printf("teste 1\n");
     unsigned char bancoPalavras[qtdPalavrasDicionario][TAM_MAX_PALAVRA];
     printf("teste 2\n");
 
-    leituraDicionarioVETOR(dic, bancoPalavras, qtdPalavrasDicionario); // escrita do dicionario na RAM
+    leituraDicionarioVETOR(dic, bancoPalavras, qtdPalavrasDicionario);
     printf("teste 3\n");
 
     unsigned char *palavra = malloc(sizeof(char) * TAM_MAX_PALAVRA); // ponteiro para teste de comparação com o dicionario
@@ -299,7 +201,7 @@ int main() {
         /*  Quando o programa detecta um caractere não valido ele reconhece
             o conjunto de caracteres anteriores como uma palavra.
             Então o ponteiro volta para a primeira posição e usa
-            as funções hamming e levenshtein
+            a função levenshtein
         */
     
         else{
@@ -330,7 +232,6 @@ int main() {
     } while(letra!=255);
     
     if(mostraNoTerminal)    printf("\n--------------------------------\n");
-
 
     printf("teste 5\n");
     fseek(arq, 0, SEEK_SET);
